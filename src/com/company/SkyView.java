e9c966a0a581c60df2d733cd01faf4c2aeee9836
BT_1N3_17 <a>
2018-11-27 11:27:26 -0500

    Done

--- src/com/company/SkyView.java
+++ src/com/company/SkyView.java
@@ -5,33 +5,35 @@ public class SkyView {
 
     public SkyView(int numRows, int numCols, double[] scanned){
         this.view = new double[numRows][numCols];
-        int currentRows = 0;
-        while(currentRows != numRows) {
-            if(currentRows % 2 == 0)
-            for (int j = 0; j < numCols; j++) {
-                        view[currentRows][j] = scanned[currentRows*numRows+j];
-                        currentRows++;
-                } else {
-                    for (int k = view[j].length - 1; k >= 0; k--) {
-                        view[j][k] = scanned[i];
-                        currentRows++;
+        int currentRow = 0;
+        while(currentRow != numRows) {
+            if(currentRow % 2 == 0) {
+                for (int j = 0; j < numCols; j++) {
+                    view[currentRow][j] = scanned[currentRow * numCols + j];
+                }
+                currentRow++;
+            }
+                else {
+                    for (int j = numCols-1;j>=0;j--)
+                    {
+                        view[currentRow][j] = scanned[currentRow*numCols+j];
                     }
+                currentRow++;
                 }
             }
         }
-    }
 
     public double getAverage(int startRow,int endRow, int startCol, int endCol){
-        double result = 0;
+        double average = 0.0;
         int count = 0;
         for(int i = startRow;i<=endRow;i++){
             for(int j = startCol;j<=endCol;j++){
-                result+=view[i][j];
+                average+=view[i][j];
                 count++;
             }
         }
-        result = result/count;
-        return result;
+        average = average/count;
+        return average;
     }
 
     public String toString(){
